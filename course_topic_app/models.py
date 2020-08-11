19c5e05aecf9e6ad7eeef55d7025e74a7de97799
donal <donalspring@gmail.com>
2020-08-11 15:38:33 +0100

    🐈 ADD - chart that works... for sure this time.... 🐈

--- course_topic_app/models.py
+++ course_topic_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class CourseTopicModel(models.Model):
     name = models.CharField(max_length=45)
-    description = models.CharField(max_length=100)
+    description = models.TextField()
     synonyms = models.CharField(max_length=100)
     parent = models.ForeignKey(to='self', on_delete=models.SET_NULL, null=True, blank=True)
 
