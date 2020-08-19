883d833385ea7bbf062a6d5d085f85ecc4f70acc
Krishna-Madhavan-07 <krishnamadhavan07@gmail.com>
2020-08-19 16:17:37 +0530

    🔥 UPDATE - RTD for the course topics service 🔥

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='lxp-course-topic-service',
-      version='1.0.3',
+      version='1.0.4',
       packages=find_packages(),
       install_requires=[
           'Django~=3.0.8',
