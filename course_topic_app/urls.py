61deba65ac84168600cc7cc8232e41181b953501
Krishna-Madhavan-07 <krishnamadhavan07@gmail.com>
2020-08-24 19:28:19 +0530

    🔥 Jaeger opentracing added 🔥

--- course_topic_app/urls.py
+++ course_topic_app/urls.py
@@ -4,5 +4,7 @@ from course_topic_app import views
 
 
 urlpatterns = [
-    path('course-topics', views.CourseTopicListView.as_view(), name='list-course-topics')
+    path('course-topics', views.CourseTopicListView.as_view(), name='list-course-topics'),
+    path('get-skills-for-course-topics', views.GetSkillsForCourseTopicsView.as_view(),
+         name='get-skills-for-course-topics')
 ]
--- course_topic_app/views.py
+++ course_topic_app/views.py
@@ -1,6 +1,14 @@
-from rest_framework import generics
+import json
+from urllib.request import urlopen
+
+from django.conf import settings
+from opentracing_instrumentation.request_context import span_in_context
+from rest_framework import generics, response
 
 from course_topic_app import serializers, models
+from lxp_course_topic_service.settings.base import tracer
+
+tracing = settings.OPENTRACING_TRACING
 
 
 class CourseTopicListView(generics.ListAPIView):
@@ -9,3 +17,21 @@ class CourseTopicListView(generics.ListAPIView):
     """
     serializer_class = serializers.CourseTopicSerializer
     queryset = models.CourseTopicModel.objects.all()
+
+
+def get_skills(request):
+    url = "http://localhost:8000/api/skills"
+    json_url = urlopen(url)
+    data = json.loads(json_url.read())
+    return data
+
+
+class GetSkillsForCourseTopicsView(generics.ListAPIView):
+    queryset = models.CourseTopicModel.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        with tracer.start_span('get_skills') as span:
+            span.set_tag('Get Skills', 'Bla bla bla')
+            with span_in_context(span):
+                data = get_skills(request)
+        return response.Response(data)
